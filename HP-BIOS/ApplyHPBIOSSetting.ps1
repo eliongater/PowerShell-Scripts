c14bbad4bf2042effe15d53caeb1bca338887ed2
eliongater <elijah@smart.co.nz>
2020-11-03 12:32:30 +1300

    Update ApplyHPBIOSSetting.ps1
    Added variable to handle Password protected BIOS' and unsecure BIOS'

--- HP-BIOS/ApplyHPBIOSSetting.ps1
+++ HP-BIOS/ApplyHPBIOSSetting.ps1
@@ -7,28 +7,49 @@ $ScriptDir = $ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPS
 $RemoteToolLocation = ""
 $PasswordLocation = ""
 $PasswordFiles = "password1.bin","Password2.bin"
-foreach($Password in $PasswordFiles){
-    $Password = $PasswordLocation+$Password
+$PasswordProtected = $true
+if($PasswordProtected){
+    foreach($Password in $PasswordFiles){
+        $Password = $PasswordLocation+$Password
+        try{
+            try{
+                #Try with local 64 bit BIOS tool
+                $result =  & "$ScriptDir\BiosConfigUtility64.exe" /setvalue:$SettingToChange,$Value /cspwdfile:$Password
+            } catch {
+                #Try with local 32 bit BIOS tool
+                $result =  & "$ScriptDir\BiosConfigUtility.exe" /setvalue:$SettingToChange,$Value /cspwdfile:$Password
+            }
+        } catch {
+            try{
+                #Try with remote 64 bit BIOS tool
+                $result =  & "$RemoteToolLocation\BiosConfigUtility64.exe" /setvalue:$SettingToChange,$Value /cspwdfile:$Password
+            } catch {
+                #Try with remote 32 bit BIOS tool
+                $result =  & "$RemoteToolLocation\BiosConfigUtility.exe" /setvalue:$SettingToChange,$Value /cspwdfile:$Password
+            }
+        }
+        #If password valid, exit loop
+        if(!($result -like "*password provided is invalid*")){
+            break
+        }
+    }
+} else {
     try{
         try{
             #Try with local 64 bit BIOS tool
-            $result =  & "$ScriptDir\BiosConfigUtility64.exe" /setvalue:$SettingToChange,$Value /cspwdfile:$Password
+            $result =  & "$ScriptDir\BiosConfigUtility64.exe" /setvalue:$SettingToChange,$Value
         } catch {
             #Try with local 32 bit BIOS tool
-            $result =  & "$ScriptDir\BiosConfigUtility.exe" /setvalue:$SettingToChange,$Value /cspwdfile:$Password
+            $result =  & "$ScriptDir\BiosConfigUtility.exe" /setvalue:$SettingToChange,$Value
         }
     } catch {
         try{
             #Try with remote 64 bit BIOS tool
-            $result =  & "$RemoteToolLocation\BiosConfigUtility64.exe" /setvalue:$SettingToChange,$Value /cspwdfile:$Password
+            $result =  & "$RemoteToolLocation\BiosConfigUtility64.exe" /setvalue:$SettingToChange,$Value
         } catch {
             #Try with remote 32 bit BIOS tool
-            $result =  & "$RemoteToolLocation\BiosConfigUtility.exe" /setvalue:$SettingToChange,$Value /cspwdfile:$Password
+            $result =  & "$RemoteToolLocation\BiosConfigUtility.exe" /setvalue:$SettingToChange,$Value
         }
-    }
-    #If password valid, exit loop
-    if(!($result -like "*password provided is invalid*")){
-        break
     }
 }
 
