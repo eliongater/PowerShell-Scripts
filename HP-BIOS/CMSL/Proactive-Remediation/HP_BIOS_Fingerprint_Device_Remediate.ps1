7c7c172c21ed07625562e8115ecec2b15f87c235
eliongater <elijah@smart.co.nz>
2021-12-10 10:30:26 +1300

    Create HP_BIOS_Fingerprint_Device_Remediate.ps1

---
+++ HP-BIOS/CMSL/Proactive-Remediation/HP_BIOS_Fingerprint_Device_Remediate.ps1
@@ -0,0 +1,33 @@
+#=============================================================================================================================
+#
+# Script Name:     HP_BIOS_Fingerprint_Device_Remediate.ps1
+# Description:     This script disables Fingerprint Device in the BIOS
+# Notes:           No variable substitution needed
+#
+#=============================================================================================================================
+
+# Define Variables
+$Setting = "Fingerprint Device"
+$ExpectedValue = "Disable"
+
+try {
+    Set-HPBIOSSettingValue -name $Setting -Value $ExpectedValue
+
+    $result = Get-HPBIOSSettingValue -name $Setting
+        
+    if ($result -eq $ExpectedValue){
+        #Exit 0 for Intune and "SUCCESS" for SCCM
+        Write-Host "SUCCESS"
+        exit 0
+    }
+    else {
+        #Exit 1 for Intune and "FAILED" for SCCM
+        Write-Host "FAILED"
+        exit 1
+    }
+}
+catch {
+    $errMsg = $_.Exception.Message
+    return $errMsg
+    exit 1
+}
