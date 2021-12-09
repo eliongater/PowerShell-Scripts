1df9aa3aa00ad770f8338a07c31c98fa43667b4b
eliongater <elijah@smart.co.nz>
2021-12-09 16:04:43 +1300

    Create HP_BIOS_SecureBoot_Detect.ps1

---
+++ HP-BIOS/CMSL/Proactive-Remediation/HP_BIOS_SecureBoot_Detect.ps1
@@ -0,0 +1,31 @@
+#=============================================================================================================================
+#
+# Script Name:     HP_BIOS_SecureBoot_Detect.ps1
+# Description:     Detect if SecureBoot is enabled
+# Notes:           Remediate if "Match" or exit code = 1
+#
+#=============================================================================================================================
+
+# Define Variables
+$Setting = "Secure Boot"
+$ExpectedValue = "Enabled"
+
+try {
+    $result = Get-HPBIOSSettingValue -name $Setting
+        
+    if ($result -eq $ExpectedValue){
+        #Exit 1 for Intune and "Match" to remediate in SCCM
+        Write-Host "Match"
+        exit 1
+    }
+    else {
+        #Exit 0 for Intune and "No_Match" for SCCM, only remediate "Match"
+        Write-Host "No_Match"
+        exit 0
+    }
+}
+catch {
+    $errMsg = $_.Exception.Message
+    return $errMsg
+    exit 1
+}
